4954251cd233632b75a01ad2817102cb267cebcf
Rahul <rd2432007@gmail.com>
2026-05-01 01:24:52 +0530

    image correction

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -4,6 +4,8 @@ import { MyContext } from "./MyContext";
 import { v1 as uuidv1 } from "uuid";
 import { useAuth } from "@clerk/react"; // ✅ যোগ হলো
 
+import logo from "./assets/Colorful_Modern_3D_Robot_Technology_Logo__2_-removebg-preview.png";
+
 function Sidebar({ isLight }) {
   const {
     allThreads,
@@ -96,7 +98,7 @@ function Sidebar({ isLight }) {
         <button onClick={createNewChat}>
           <div className="col-2 logoBrand">
             <img
-              src="src\assets\Colorful_Modern_3D_Robot_Technology_Logo__2_-removebg-preview.png"
+              src={logo}
               alt="BuddyAI logo"
               className="logo"
             />
